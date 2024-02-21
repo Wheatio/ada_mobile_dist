a7c0365bfe5434e98d6215dcfecc1d578c358e8d
Chris Mac <cjw@marlboroughcollege.org>
2024-02-21 14:11:50 +0000

    changed icons

--- assets/fontawesome-pro.6a55b153.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.3e2de913.js";var o=a(async()=>{});export{o as default};
---
+++ assets/fontawesome-pro.9dc342b3.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.a460bdca.js";var o=a(async()=>{});export{o as default};
